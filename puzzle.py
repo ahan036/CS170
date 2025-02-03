c062b2e3fe40e30d3b3539619cd68132b54046f3
Ashley Han <peachyyy@Ashleys-Air-5.frontiernet.net>
2025-02-02 19:32:00 -0800

    finished and tested search

--- puzzle.py
+++ puzzle.py
@@ -3,13 +3,14 @@ import copy
 
 def main():
     puzzle_mode = input("Welcome to Ashley's puzzle solver. Type '1' to use a default puzzle, or '2' to create your own \n")
-
-    if puzzle_mode == 1:
+    if puzzle_mode == '1':
         #select algo chooses between: uniform, misplaced, manhattan
         #default puzzles are the different difficulty of puzzles
-        default_puzzles()
+        puzzle = default_puzzles()
+        heuristic = input("Select algorithm. (1) uniform cost search (2) A* Misplaced Tile Heuristic (3) A* Manhattan Distance Heuristic")
+        a_star_search(puzzle, heuristic)
 
-    if puzzle_mode == 2:
+    if puzzle_mode == '2':
         print("Enter your puzzle, using zero to represent the blank. Only enter valid 8-puzzles. Separate the numbers with a SPACE and press RET when finished")
         puzzle_row_one = input("Enter the first row: ")
         puzzle_row_two = input("Enter the second row: ")
@@ -26,42 +27,69 @@ def main():
 
         user_puzzle = [puzzle_row_one, puzzle_row_two, puzzle_row_three]
         #select_algo(user_puzzle)
-
     return
-    
+
+def a_star_search(state, heuristic):  
+#need to evaluate which state is the most promising 
+#f = cost + estimated cost 
+    starting_node = Puzzle(state)
+    starting_node.cost = select_algo(state, heuristic)
+    working_queue = []
+    repeated_states = set()
+    min_heap.heappush(working_queue, starting_node)
+    num_nodes_expanded = 0
+    max_queue_size = 0
+    repeated_states.add(tuple(map(tuple, starting_node.state)))
+
+    while len(working_queue) > 0: 
+        max_queue_size = max(len(working_queue), max_queue_size)
+        node_from_queue = min_heap.heappop(working_queue)
+        if node_from_queue.state == goalState: 
+            print("Number of nodes expanded: ", num_nodes_expanded)
+            print("Max queue size: ", max_queue_size)
+            return node_from_queue
+        num_nodes_expanded +=1
+        for child in expand(node_from_queue):
+            child = tuple(map(tuple, child))
+            if child not in repeated_states:
+                repeated_states.add(child)
+                child_puzzle = Puzzle(child)
+                child_puzzle.depth = node_from_queue.depth + 1
+                child_puzzle.cost = child_puzzle.depth + select_algo(child_puzzle.state, heuristic)
+                min_heap.heappush(working_queue, child_puzzle)
+
 #this menu needs to be changed 
-def select_algo(puzzle):
-    algorithm = input("Select algorithm. (1) uniform cost search (2) A* Misplaced Tile Heuristic (3) A* Manhattan Distance Heuristic")
-    if algorithm == 1:
-        uniform_cost_search(puzzle)
-    if algorithm == 2:
+def select_algo(puzzle, algorithm):
+    if algorithm == '1':
+        return uniform_cost_search(puzzle)
+    if algorithm == '2':
         #change this when we implement search
-        heuristic = misplaced_tile(puzzle)
-    if algorithm == 3:
+        return misplaced_tile(puzzle)
+    if algorithm == '3':
         #change this when we implement search
-        heuristic = manhattan_heuristic(puzzle)
+        return manhattan_heuristic(puzzle)
 
 def default_puzzles():
     difficulty = input(
         "Please enter a desired difficulty on a scale from 0 to 5. \n")
-    if difficulty == 0:
+    if difficulty == '0':
         print("Difficulty of 'Trivial' selected.")
-        select_algo(trivial)
-    if difficulty == 1:
+        return trivial
+    if difficulty == '1':
         print("Difficulty of 'Very Easy' selected.")
-        select_algo(veryEasy)
-    if difficulty == 2:
+        return veryEasy
+    if difficulty == '2':
         print("Difficulty of 'Easy' selected.")
-        select_algo(easy)
-    if difficulty == 3:
+        return easy
+    if difficulty == '3':
         print("Difficulty of 'Medium' selected.")
-        select_algo(medium)
-    if difficulty == 4:
+        return medium
+    if difficulty == '4':
         print("Difficulty of 'Hard' selected.")
-        select_algo(hard)
-    if difficulty == 5:
+        return hard
+    if difficulty == '5':
         print("Difficulty of 'Impossible' selected.")
-        select_algo(impossible)
+        return impossible
     
 
 def print_puzzle(puzzle):
@@ -71,9 +99,9 @@ def print_puzzle(puzzle):
 
 
 #default puzzle options
-trivial = [[1, 2, 3],
-[4, 5, 6],
-[7, 8, 0]]
+trivial = ((1, 2, 3),
+(4, 5, 6),
+(7, 8, 0))
 
 veryEasy = [[1, 2, 3],
 [4, 5, 6],
@@ -98,9 +126,9 @@ impossible = [[8, 1, 2],
 [7, 6, 5]]
 
 #compare this to every state to see if the puzzle is finished 
-goalState = [[1, 2, 3], 
-             [4, 5, 6], 
-             [7, 8, 0]]
+goalState = ((1, 2, 3), 
+             (4, 5, 6), 
+             (7, 8, 0))
 
 def goal_pos(goalState, state): 
     for i in range(3):
@@ -116,7 +144,6 @@ def manhattan_heuristic(state):
             if state[i][j] != 0:
                 goal_row, goal_column = goal_pos(goalState, state[i][j])
                 distance += abs(i - goal_row) + abs(j - goal_column)
-    print('Manhatten: ' + str(distance)) #testing
     return distance
 
 def misplaced_tile(state):
@@ -127,7 +154,6 @@ def misplaced_tile(state):
             if state[i][j] != 0:
                 if(goalState[i][j] != state[i][j]):
                     count += 1
-    print('misplaced tiles: ' + str(count))
     return count
 
 #provides no heuristic for the search to work with 
@@ -138,7 +164,6 @@ def uniform_cost_search(puzzle):
 class Puzzle:
     def __init__(self, state, parent = None, depth = 0, cost = 0):
         self.state = state
-        self.parent = parent 
         self.depth = depth
         self.cost = cost
 
@@ -149,12 +174,14 @@ class Puzzle:
         return self.state == other.state
 
 #find the possible children and choose one
-def expand(state):
+def expand(puzzle):
+    children = []
 #find where the zero is first 
-    zero_row, zero_col = 0
+    zero_row = 0
+    zero_col = 0
     for i in range(3):
         for j in range (3):
-            if state[i][j] == 0:
+            if puzzle.state[i][j] == 0:
                 zero_row = i
                 zero_col = j
 #check each possible move, up, down, left, right 
@@ -162,53 +189,24 @@ def expand(state):
 #then we run the heuristic on the children to see which is best 
 # we repeat this until goal state = state 
     if zero_row > 0: #move down
-        move(state, zero_row - 1, zero_col, zero_row, zero_col)
+        children.append(move(puzzle.state, zero_row - 1, zero_col, zero_row, zero_col))
     if zero_row < 2: #move up 
-        move(state, zero_row + 1, zero_col, zero_row, zero_col)
+        children.append(move(puzzle.state, zero_row + 1, zero_col, zero_row, zero_col))
     if zero_col > 0: #move left
-        move(state, zero_row, zero_col - 1, zero_row, zero_col)
+        children.append(move(puzzle.state, zero_row, zero_col - 1, zero_row, zero_col))
     if zero_col < 2: #move right
-        move(state, zero_row, zero_col + 1, zero_row, zero_col)
+        children.append(move(puzzle.state, zero_row, zero_col + 1, zero_row, zero_col))
+    return children
 
 #create a copy of existing state and swap the values
 def move(state, new_row, new_col, old_row, old_col):
     child = copy.deepcopy(state)
-    child[old_row][old_col] = child[new_row][new_col]
-    child[new_row][new_col] = 0
-
-
-
-
-def a_star_search(state, heuristic):  
-#need to evaluate which state is the most promising 
-#f = cost + estimated cost 
-    starting_node = Puzzle(state, cost = heuristic(state))
-    working_queue = []
-    repeated_states = dict()
-    min_heap.heappush(working_queue, starting_node)
-    num_nodes_expaned = 0
-    max_queue_size = 0
-    repeated_states[starting_node]
-
-    while len(working_queue) > 0: 
-        max_queue_size = max(len(working_queue), max_queue_size)
-        node_from_queue = min_heap.heappop(working_queue)
-        state_key = tuple(map(tuple, node_from_queue.state))
-        if state_key in repeated_states:
-            continue
-        repeated_states[state_key] = True
-
-        if node_from_queue.state == goalState: 
-            print("Number of nodes expanded: ", num_nodes_expanded)
-            print("Max queue size: ", max_queue_size)
-            return node_from_queue
-        
-        num_nodes_expanded +=1
-        for child in expand(node_from_queue):
-            child.cost = child.depth + heuristic(child.state)
-            min_heap.heappush(working_queue, child)
-
-
+    child_list = list(map(list, child))
+    child_list[old_row][old_col] = child_list[new_row][new_col]
+    child_list[new_row][new_col] = 0
+    child = tuple(map(tuple, child_list))
+    return child
+main()
 
 
 #to move the blank space around the puzzle
